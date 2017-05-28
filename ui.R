23d7f4021850861e8725c588639c38fc1b3a7745
zyzai605 <54966999@qq.com>
2017-05-29 00:42:37 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,21 +1,25 @@
-library(shiny)
-library(ggplot2)
- 
-dataset <- iris
- 
-shinyUI(pageWithSidebar(
- 
-  headerPanel("数据展示"),
-  sidebarPanel(
- 
-    selectInput('x', 'X', names(dataset)),
-    selectInput('y', 'Y', names(dataset)[2]),
-    selectInput('color', 'Color', c('None', names(dataset))),
- 
-    checkboxInput('smooth', 'Smooth')
-    ),
- 
-  mainPanel(
-    plotOutput('plot')
+shinyUI(bootstrapPage(
+  
+  selectInput(inputId = "n_breaks",
+              label = "Number of bins in histogram (approximate):",
+              choices = c(10, 20, 35, 50),
+              selected = 20),
+  
+  checkboxInput(inputId = "individual_obs",
+                label = strong("Show data1"),
+                value = FALSE),
+  
+  checkboxInput(inputId = "density",
+                label = strong("Show data2"),
+                value = FALSE),
+  
+  plotOutput(outputId = "main_plot", height = "300px"),
+  
+  # Display this only if the density is shown
+  conditionalPanel(condition = "input.density == true",
+                   sliderInput(inputId = "bw_adjust",
+                               label = "Bandwidth adjustment:",
+                               min = 0.2, max = 2, value = 1, step = 0.2)
   )
+  
 ))
